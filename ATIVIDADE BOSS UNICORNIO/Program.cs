2a8cc4f17ae231988e1348ae00c6b72b6ed2272a
Guizoo0 <143220649+Guizoo0@users.noreply.github.com>
2023-08-25 16:58:51 -0300

    atividade de boss
    atividade extra passada em aula pelo fascine

---
+++ ATIVIDADE BOSS UNICORNIO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATIVIDADE_BOSS_UNICORNIO
+{
+    internal class Program
+    {
+        static void Main(string[] args, string palavra)
+        {
+            string palavra = "";
+            Console.Write("DIGITE SEU NOME: ");
+            string palavra = Console.ReadLine();
+            
+                
+                
+               
+              
+
+
+
+
+
+
+
+
+
+      
+
+        }
+    }
+}
